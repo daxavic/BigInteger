39dceb9ccf09d4775de06234b7dca79614317e21
daviktorova <daviktorova@mail.ru>
2019-03-19 02:11:03 +0300

    BidInteger Java class

--- src/main/BigInteger/BigInteger.java
+++ src/main/BigInteger/BigInteger.java
@@ -24,8 +24,9 @@ public final class BigInteger {
 
     //создаём конструктор из строки
      BigInteger(String val) {
-        if ( !val.matches("[0-9]+") )
+        if ( !val.matches("[0-9]+") ) {
             throw new IllegalArgumentException("Число введено неверно");
+        }
         String[] splitNumber = val.split("");
         for ( int i = 0; i < splitNumber.length; i++ ) {
             byteEl.add(0, new Byte(splitNumber[ i ]));
@@ -35,25 +36,29 @@ public final class BigInteger {
 
 
     // Константы:
-    private final int MAX_ELEMENT = 9; // максимальное число в ячейке массива
-    private final byte MIN_ELEMENT =(byte)0; // минимальное число в ячейке массива
+    private final byte MIN_ELEMENT = 0; // минимальное число в ячейке массива
     private final byte ELEMENT_BASE = 10;
-    //private final Byte[] ZERO = {0};
-    //private final Byte[] ONE = {1};
 
     // метод "Сложение"
-    private ArrayList <Byte> plusHelp(ArrayList <Byte> max, ArrayList <Byte> min) {
+    private ArrayList <Byte> plusHelp(ArrayList <Byte> first, ArrayList <Byte> second) {
+        ArrayList<Byte> max;
+        ArrayList<Byte> min ;
+        if (first.size() > second.size()){max = new ArrayList <>(first);
+                                          min = new ArrayList <>(second);}
+         else {
+             max = new ArrayList <>(second);
+             min = new ArrayList <>(first);}
         int maxSize = max.size();
         int minSize = min.size();
         int count;
         int nextRank = 0; // переменная, в которой хранится число переносящееся в след. рязряд.
         ArrayList <Byte> result = max;
-        for ( int i = 0; i < minSize; i++ ) {
+        for ( int i = 0; i < minSize; i++ ) {           // складываем пока длина чисел одинаковая
             count = (max.get(i) + min.get(i) + nextRank);
             nextRank = count/ELEMENT_BASE;
-            result.set(i, (byte)(count % ELEMENT_BASE ));
+            Byte set = result.set(i , (byte) (count % ELEMENT_BASE));
         }
-        for ( int i = minSize; i < maxSize; i++ ) {
+        for ( int i = minSize; i < maxSize; i++ ) {     // прибавляем излишек из предыдущего разряда
             if ( nextRank == 0 ) break;
             count = max.get(i) + 1;
             nextRank = count/ELEMENT_BASE;
@@ -66,10 +71,7 @@ public final class BigInteger {
 
     public String plus(BigInteger this,BigInteger other) {
         ArrayList <Byte> result;
-        if ( equality(other) == 1 )
-            result = plusHelp(this.byteEl, other.byteEl);
-        else
-            result = plusHelp(other.byteEl, this.byteEl);
+        result = plusHelp(this.byteEl, other.byteEl);
         if ( toString(result) == "" ) return "0";
         else
             return toString(result);
@@ -78,34 +80,31 @@ public final class BigInteger {
 
     // Метод "Вычитание"
     private ArrayList <Byte> minusHelp(ArrayList <Byte> minuend, ArrayList <Byte> subtrahend) {
-        int lastRank = 0; // переменная, в которой хранится число переносящееся в пред. рязряд.
+        int nextRank = 0; // переменная, в которой хранится число переносящееся в пред. рязряд.
         int count;
-        ArrayList <Byte> result = minuend;
+        ArrayList <Byte> result =new ArrayList <>(minuend);
         int minSize = minuend.size();
         int subSize = subtrahend.size();
         if ( minSize < subSize ) throw new IllegalArgumentException("Число не может быть отрицательным");
-        for ( int i = 0; i < subSize; i++ ) {
-            count = minuend.get(i) - subtrahend.get(i) - lastRank;
+        for ( int i = 0; i < subSize; i++ ) {                        // вычитаем пока длина чисел одинаковая
+            count = minuend.get(i) - subtrahend.get(i) - nextRank;
             if ( count < MIN_ELEMENT ) {
-                result.set(i, (byte) (count + ELEMENT_BASE));
-                lastRank = 1;
-            } else {
-                result.set(i, (byte) count);
-                lastRank = 0;
-            }
+                count += ELEMENT_BASE;
+                nextRank = 1;}
+             else nextRank = 0;
+             result.set(i, (byte)count);
         }
-        for ( int i = subSize; i < minSize; i++ ) {
-            if ( lastRank == 0 ) {
+        for ( int i = subSize; i < minSize; i++ ) {                 // вычитаем заимствованную меньшими разрядами 1
+            if ( nextRank == 0 ) {
                 break;
             }
-            count = minuend.get(i) - lastRank;
-            lastRank = 0;
-            if ( count < MIN_ELEMENT ) {
-                lastRank = 1;
-                result.set(i, (byte) (count + ELEMENT_BASE));
-            }
+            count = minuend.get(i) - nextRank;
+            if ( count >= MIN_ELEMENT ) {
+                nextRank = 0;}
+              else count += ELEMENT_BASE;
+             result.set(i, (byte) count);
         }
-        if ( lastRank == 1 ) throw new IllegalArgumentException("Число не может быть отрицательным");
+        if ( nextRank == 1 ) throw new IllegalArgumentException("Число не может быть отрицательным");
         return result;
     }
 
@@ -119,18 +118,21 @@ public final class BigInteger {
 
 
     // метод: "Умножение" (умножение столбиком)
+    // multiplyHelp умножает число на одну цифру
     private ArrayList <Byte> multiplyHelp(ArrayList <Byte> multiplier1, byte digit, int n) {
-        ArrayList <Byte> result = null;
-        int nextRank = 0;
+        ArrayList<Byte> result = new ArrayList<Byte>();
+        int nextRank = 0;  // переменная, в которой хранится число переносящееся в пред. рязряд.
         int countor;
-        for ( int i = 0; i < multiplier1.size(); i++ ) {
-            countor = digit * multiplier1.get(i) + nextRank;
-            nextRank = 0;
-            if ( countor > MAX_ELEMENT ) {
-                nextRank = countor / 10;
-            }
-            result.add((byte) (countor % 10));
+        int m = 0;
+        for ( int i = 0; i < n ; i++ ) {
+            result.add(0, (byte) 0);
+            m++;
         }
+        for ( int i = n; i < multiplier1.size(); i++ ) {
+            countor = digit * multiplier1.get(i) + nextRank;
+            nextRank = countor / ELEMENT_BASE;
+            countor %= ELEMENT_BASE;
+            result.add((byte) countor );}
         if ( nextRank != 0 ) {
             result.add((byte) nextRank);
         }
@@ -138,101 +140,110 @@ public final class BigInteger {
     }
 
     public String multiply(BigInteger other) {
-        ArrayList <Byte> multiplier = this.byteEl;
-        int n = 0; // показывет сколько нулей долбавлено в множитель
-        ArrayList <Byte> result = null;
+        ArrayList <Byte> multiplier = this.byteEl;       /* промежуточное число, на которое
+                                                        умножается цифра из второго множителя*/
+        int n = 0;                                      // показывет сколько нулей долбавлено в множитель
+        ArrayList <Byte> result = new ArrayList <Byte>();
         for ( int i = 0; i < other.bigSize; i++ ) {
-            if ( other.byteEl.get(i) == 0 ) {
+           if ( other.byteEl.get(i) == 0 ) {            // пропускаем умножение, если множитель равен 0
                 multiplier.add(0, (byte) 0);
                 n++;
                 continue;
             }
             if ( other.byteEl.get(i) == 1 ) {
-                result = plusHelp(result, this.byteEl);
+                result = plusHelp(result, multiplier);
             } else {
                 result = plusHelp(result, multiplyHelp(multiplier, other.byteEl.get(i), n));
-                n++;
-                multiplier.add(0, (byte) 0);
             }
+            multiplier.add(0, (byte) 0);
+            n++;
         }
-        return toString(result);
+        if ( toString(result).isEmpty() ) return "0";
+        else
+            return toString(result);
     }
 
 
-    // метод "Деление" (деление столбиком)
-    public String div(BigInteger other) {
-        ArrayList <Byte> controller = this.byteEl;
-        ArrayList <Byte> result = new ArrayList <>(0);
+    // метод "Деление"/ Остаток (деление столбиком)
+    public String div (BigInteger other) {
+        ArrayList <Byte> controller = new ArrayList <>(this.byteEl);
+        ArrayList<Byte> maxDivinder ;
+        ArrayList<Byte> divinder = new ArrayList <>(other.byteEl) ;    // делитель
+        ArrayList <Byte> dividend = new ArrayList <Byte>();      // число, из которого вычитаем
+        int n;                                              // сколько раз вычитаем число
+        ArrayList <Byte> result = new ArrayList <>();
         ArrayList <Byte> zero = new ArrayList <Byte>();
         zero.add((byte) 0);
         ArrayList <Byte> first = new ArrayList <Byte>();
         first.add((byte) 1);
-        int n; // сколько раз вычитаем число
-        int i = this.bigSize; // показывает индекс ячейки делимого
-        ArrayList <Byte> dividend = new ArrayList <Byte>();// число, из которого вычитаем
         if ( other.byteEl == zero ) throw new IllegalArgumentException("Нельзя делить на ноль");
         if ( other.byteEl == first ) return toString(this.byteEl);
-        int equal = equalityHelp(this.byteEl, other.byteEl);
-        if ( equal == 2 ) {
-            return "0";
-        } else {
-            if ( equal == 0 ) {
-                return "1";
-            }
-        }
-        while ( equalityHelp(controller, other.byteEl) != 2 ) {
-            while ( equalityHelp(dividend, other.byteEl) == 2 ) {
-                i--;
-                dividend.add(0, this.byteEl.get(i));
-                controller.remove(i);
-            }
+        int equal = equalityHelp(this.byteEl, other.byteEl); // проверяет на равенство делитель и делимое
+        if ( equal == 2 ) return "0";
+        else if ( equal == 0 ) return "1";
+        for (int i = controller.size() - 1; i >= 0; i--) {
             n = 0;
-            while ( equalityHelp(dividend, other.byteEl) == 1 ) {
-                dividend = minusHelp(dividend, other.byteEl);
+            maxDivinder = new ArrayList <>(divinder);
+            dividend.add(0, controller.get(i));
+            if (equalityHelp(dividend,divinder) == 2) {result.add(0, (byte) n);
+                continue;}
+            n++;
+            if (zerosDeath(dividend).isEmpty()) dividend = new ArrayList <>(zero);
+            while ( equalityHelp(dividend,maxDivinder) != 2) {
                 n++;
+                maxDivinder = plusHelp(maxDivinder,divinder);
             }
-            result.add(0, (byte) n);
+            n--;
+            maxDivinder = zerosDeath(minusHelp(maxDivinder,divinder));
+
+            dividend = zerosDeath(minusHelp(dividend, maxDivinder));
+            result.add(0,(byte) n);
         }
-        System.out.println(result);
-        return toString(result);
+        result = zerosDeath(result);
+        if ( toString(result).isEmpty() ) return "0";
+        else
+            return toString(result);
     }
 
-    // метод "остаток" (деление столбиком)
+    // метод "остаток"/"" (остаток деление столбиком)
     public String mod(BigInteger other) {
-        ArrayList <Byte> controller = this.byteEl;
-        ArrayList <Byte> result = null;
-        result.add((byte) 0);
+
+        ArrayList <Byte> controller = new ArrayList <>(this.byteEl);
+        ArrayList<Byte> maxDivinder ;
+        ArrayList<Byte> divinder = new ArrayList <>(other.byteEl) ;    // делитель
+        ArrayList <Byte> dividend = new ArrayList <Byte>();      // число, из которого вычитаем
+        int n;                                              // сколько раз вычитаем число
+        ArrayList <Byte> result = new ArrayList <>();
         ArrayList <Byte> zero = new ArrayList <Byte>();
         zero.add((byte) 0);
-        ArrayList <Byte> first = new ArrayList <Byte>();
-        first.add((byte) 0);
-        int n; // сколько раз вычитаем число
-        int i = this.bigSize; // показывает индекс ячейки делимого
-        ArrayList <Byte> dividend = new ArrayList <Byte>();// число, из которого вычитаем
+        //ArrayList <Byte> first = new ArrayList <Byte>();
+        //first.add((byte) 1);
         if ( other.byteEl == zero ) throw new IllegalArgumentException("Нельзя делить на ноль");
-        if ( other.byteEl == first ) return toString(this.byteEl);
-        int equal = equalityHelp(this.byteEl, other.byteEl);
-        if ( equal == 2 ) {
-            return "0";
-        } else {
-            if ( equal == 0 ) {
-                return "1";
-            }
-        }
-        while ( equalityHelp(controller, other.byteEl) != 2 ) {
-            while ( equalityHelp(dividend, other.byteEl) == 2 ) {
-                i--;
-                dividend.add(0, this.byteEl.get(i));
-                controller.remove(i);
-            }
+       // if ( other.byteEl == first ) return toString(this.byteEl);
+        int equal = equalityHelp(this.byteEl, other.byteEl); // проверяет на равенство делитель и делимое
+        if ( equal == 0 ) return "0";
+        for (int i = controller.size() - 1; i >= 0; i--) {
             n = 0;
-            while ( equalityHelp(dividend, other.byteEl) == 1 ) {
-                dividend = minusHelp(dividend, other.byteEl);
+            maxDivinder = new ArrayList <>(divinder);
+            dividend.add(0, controller.get(i));
+            if (equalityHelp(dividend,divinder) == 2) {result.add(0, (byte) n);
+                continue;}
+            n++;
+            if (zerosDeath(dividend).isEmpty()) dividend = new ArrayList <>(zero);
+            while ( equalityHelp(dividend,maxDivinder) != 2) {
                 n++;
+                maxDivinder = plusHelp(maxDivinder,divinder);
             }
-            result.add(0, (byte) n);
+            n--;
+            maxDivinder = zerosDeath(minusHelp(maxDivinder,divinder));
+
+            dividend = zerosDeath(minusHelp(dividend, maxDivinder));
+            result.add(0,(byte) n);
         }
-        return toString(dividend);
+       dividend = zerosDeath(dividend);
+        if ( toString(dividend).isEmpty() ) return "0";
+        else
+            return toString(dividend);
     }
 
 
@@ -270,16 +281,17 @@ public final class BigInteger {
     }
 
     // метод, убирающий лишние нули
-    private void zerosDeath(ArrayList <Byte> val) {
+    private ArrayList <Byte> zerosDeath(ArrayList <Byte> val) {
         int k = 0;  // переменная, показывающая нули, идущие перед числом
         int i;
         for ( i = val.size() - 1; k == 0 && i>=0; i-- ) {
             if ( val.get(i) == k ) val.remove(i);
             else k = 1;
         }
+        return val;
     }
 
-    // превращение массив в строку
+    // превращение массива в строку
     public String toString(ArrayList <Byte> val) {
         String result = new String();
         zerosDeath(val);
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,10 +1,22 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import java.util.ArrayList;
 
 
 public class Tests {
+   static void equalityTest(int expected, String lef, String rig){
+       BigInteger obj1 = new BigInteger(lef);
+       BigInteger obj2 = new BigInteger(rig);
+       int result = obj1.equality(obj2);
+        assertEquals(expected, result);
+    }
+    @Test
+    public void equality () {
+       equalityTest(0, "123456789987654321", "123456789987654321");
+       equalityTest(1, "9978767567564543453423445678989896756543", "99787675675645434534234456789898");
+       equalityTest(2, "111111111187654321", "123456789987654321");
+
+    }
 
         static void plusTest (String expected, String lef, String rig){
         BigInteger obj1 = new BigInteger(lef);
@@ -24,6 +36,7 @@ public class Tests {
         plusTest("4", "2", "2");
         plusTest("48", "24", "24");
         plusTest("91288568294", "91217443908", "71124386");
+            plusTest("121932631112635269", "98765432100000000", "23167199012635269");
     }
 
     static void minusTest(String expect, String lef, String rit) {
@@ -39,6 +52,8 @@ public class Tests {
         minusTest("3", "7", "4");
         minusTest("2", "4", "2");
         minusTest("0", "117", "117");
+        minusTest("909", "1008", "99");
+        minusTest("786456341510091", "786545342498978", "89000988887");
 
     }
 
@@ -51,11 +66,14 @@ public class Tests {
 
     @Test
     public void  multiply() {
-        multiplyTest("81", "9", "9");
-        multiplyTest("28", "7", "4");
+        multiplyTest("51948", "999", "52");
         multiplyTest("4", "2", "2");
         multiplyTest("500", "100", "5");
         multiplyTest("0", "1", "0");
+        multiplyTest("81", "9", "9");
+        multiplyTest("121932631112635269", "987654321", "123456789");
+        multiplyTest("75892268083671675044", "7689509798", "9869584678");
+        multiplyTest("839416315003641515507739245048705929950", "8504830679486059757485", "98698768574939670");
     }
 
     static void  divTest(String expect, String lef, String rit) {
@@ -67,8 +85,33 @@ public class Tests {
 
     @Test
     public void  div() {
+        divTest("102", "510", "5");
+        divTest("0", "9999", "99999999");
+        divTest("1", "9999", "9999");
+        divTest("114", "9999", "87");
+        divTest("102", "510", "5");
         divTest("0", "0", "1");
         divTest("100", "500", "5");
-        divTest("14", "9999", "87");
+        divTest("845850160668657", "73247965406850780697065", "86596857");
+
+    }
+
+    static void  modTest(String expect, String lef, String rit) {
+        BigInteger obj1 = new BigInteger(lef);
+        BigInteger obj2 = new BigInteger(rit);
+        String result = obj1.mod(obj2);
+        assertEquals(expect, result);
+    }
+
+    @Test
+    public void  mod() {
+       modTest("3767", "495839", "23432");
+       modTest("0" , "510" , "5");
+       modTest("34" , "1000934" , "100");
+       modTest("34" , "1000934" , "100");
+       modTest("0" , "999" , "999");
+       modTest( "8", "495839", "21");
+
     }
 }
+
